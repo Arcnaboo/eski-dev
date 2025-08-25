cd267aab5e34197b229997564cf47f285cd3344a
Arcnaboo <aakgur@gmail.com>
2025-08-25 19:24:11 +0300

    first

--- Gold.Core/obj/Debug/net5.0/Gold.Core.AssemblyInfo.cs
+++ Gold.Core/obj/Debug/net5.0/Gold.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gold.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c0fe4d77176d97b0fa19e2bfd2bd97af284a730")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gold.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gold.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Gold.Domain/obj/Debug/net5.0/Gold.Domain.AssemblyInfo.cs
+++ Gold.Domain/obj/Debug/net5.0/Gold.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gold.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c0fe4d77176d97b0fa19e2bfd2bd97af284a730")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gold.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gold.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
